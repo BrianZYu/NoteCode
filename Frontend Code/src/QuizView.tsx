76cd97337840329dcf14cf016108d97ebe8848c2
april-yyt <aprilytyang@gmail.com>
2022-10-25 14:52:40 -0700

    change color to orange

--- Frontend Code/src/QuizView.tsx
+++ Frontend Code/src/QuizView.tsx
@@ -5,6 +5,7 @@ import MarkdownTextView from './Components/MarkdownTextView';
 import { Question, QuizMetadata } from './Data Structures';
 import './QuizView.css'
 import './quizdesign.json'
+import {useState} from 'react';
 
 interface QuizViewProps {
     metadata: QuizMetadata
@@ -24,7 +25,9 @@ class QuizView extends React.Component<QuizViewProps, QuizViewState> {
         this.state = {
             
         };
+       
     }
+    
     render() {
 
         // Here is an example of how you can insert a variable number of elements into a page
@@ -38,8 +41,6 @@ class QuizView extends React.Component<QuizViewProps, QuizViewState> {
             <div className='quiz-container'>
                 <h2 className='quiz-title'><MarkdownTextView rawText={this.props.metadata.quiz_title} /></h2>
                 <MarkdownTextView rawText={this.props.metadata.quiz_subtitle || undefined} />
-
-
                     <div className='multi-select'>
                         <h3>Which computer languages have you used?</h3>
                             <div className='multi-options'>
@@ -51,47 +52,6 @@ class QuizView extends React.Component<QuizViewProps, QuizViewState> {
                                 <label className='multi-option'><input type="checkbox" name="langs" value="LISP"/><span className="checkmark"></span>LISP</label>
                             </div>
                     </div>
-
-                    {/* <div className='multi-select'>
-
-                    <div className="row"> 
-                    <div className="col-12"> 
-                        <p className="fw-bold"> Which computer languages have you used? </p>
-                            <div className='multi-choices'> </div>
-                            <input type="checkbox" name="langs" id="one" value="JavaScript"/>
-                            <input type="checkbox" name="langs" id="two" value="Java"/>
-                            <input type="checkbox" name="langs" id="three" value="C"/>
-                            <input type="checkbox" name="langs" id="four" value="Python"/>
-                            <input type="checkbox" name="langs" id="five" value="Ook"/>
-                            <input type="checkbox" name="langs" id="six" value="LISP"/>
-
-                            <label htmlFor="one" className="box first"> 
-                            <div className="course"> <span className="circle"></span> <span className="subject"> JavaScript </span> </div> 
-                            </label> 
-
-                            <label htmlFor="two" className="box first"> 
-                            <div className="course"> <span className="circle"></span> <span className="subject"> Java </span> </div> 
-                            </label> 
-
-                            <label htmlFor="three" className="box first"> 
-                            <div className="course"> <span className="circle"></span> <span className="subject"> C </span> </div> 
-                            </label> 
-
-                            <label htmlFor="four" className="box first"> 
-                            <div className="course"> <span className="circle"></span> <span className="subject"> Python </span> </div> 
-                            </label> 
-
-                            <label htmlFor="five" className="box first"> 
-                            <div className="course"> <span className="circle"></span> <span className="subject"> Ook </span> </div> 
-                            </label> 
-
-                            <label htmlFor="six" className="box first"> 
-                            <div className="course"> <span className="circle"></span> <span className="subject"> LISP </span> </div> 
-                            </label>
-                    </div> 
-                    </div> 
-                    </div> */}
-                
                 {questions}
             </div>
 
